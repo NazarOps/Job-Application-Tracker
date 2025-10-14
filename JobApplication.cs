989ad84565ff71a8d8b9f260c23431e553d5464e
Nazar Kokovkin <lilnz4@hotmail.com>
2025-10-14 15:51:22 +0200

    Add project files.

---
+++ JobApplication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Job_Application_Tracker
+{
+    internal class JobApplication
+    {
+        public string CompanyName;
+        public string PositionTitle;
+        public Status Status;
+        public DateTime ApplicationDate;
+        public DateTime? ResponseDate;
+        public int SalaryExpectation;
+
+        public void GetDaysSinceApplied()
+        {
+
+        }
+
+        public void GetSummary()
+        {
+
+        }
+    }
+}
